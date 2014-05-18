dfaa05746071c8b7c4532e3907eb696e2e7b4174
cliff xuan <cliff.xuan@gmail.com>
2014-05-18 23:44:19 +0100

    simplified cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,52 +1,28 @@
 #!/bin/sh
 BASE_DIR=$(cd "$(dirname "$0")"; pwd)
 
-if [ $(readlink -f $HOME/.vim) == "$HOME/dotfiles/vimfiles" ]
-then
-    echo "remove .vim"
-    rm -rf ~/.vim
-fi
+echo "remove .vim"
+rm -rf ~/.vim
 
-if [ $(readlink -f $HOME/.vimrc) == "$HOME/dotfiles/vimfiles/_vimrc" ]
-then
-    echo "remove .vimrc"
-    rm -rf ~/.vimrc
-fi
+echo "remove .vimrc"
+rm -rf ~/.vimrc
 
-if [ $(readlink -f $HOME/.gitconfig) == "$HOME/dotfiles/gitconfig" ]
-then
-    echo "remove .gitconfig"
-    rm -rf ~/.gitconfig
-fi
+echo "remove .gitconfig"
+rm -rf ~/.gitconfig
 
-if [ $(readlink -f $HOME/.oh-my-zsh) == "$HOME/dotfiles/oh-my-zsh" ]
-then
-    echo "remove .oh-my-zsh"
-    rm -rf ~/.oh-my-zsh
-fi
+echo "remove .oh-my-zsh"
+rm -rf ~/.oh-my-zsh
 
-if [ $(readlink -f $HOME/.zshrc) == "$HOME/dotfiles/oh-my-zsh/zshrc" ]
-then
-    echo "remove .zshrc"
-    rm -rf ~/.zshrc
-fi
+echo "remove .zshrc"
+rm -rf ~/.zshrc
 
-if [ $(readlink -f $HOME/.bash_it) == "$HOME/dotfiles/bash-it" ]
-then
-    echo "remove .bash_it"
-    rm -rf ~/.bash_it
-fi
+echo "remove .bash_it"
+rm -rf ~/.bash_it
 
-if [ $(readlink -f $HOME/.bash_profile) == "$HOME/dotfiles/bash-it/template/bash_profile.cliff.template.bash" ]
-then
-    echo "remove .bash_profile"
-    rm -rf ~/.bash_profile
-    echo "added back a minimux bash_profile"
-    cp $BASE_DIR/bash-it/template/bash_profile.minimum.bash $HOME/.bash_profile
-fi
+echo "remove .bash_profile"
+rm -rf ~/.bash_profile
+echo "added back a minimux bash_profile"
+cp $BASE_DIR/bash-it/template/bash_profile.minimum.bash $HOME/.bash_profile
 
-if [ $(readlink -f $HOME/.tmux.conf) == "$HOME/dotfiles/tmux.conf" ]
-then
-    echo "remove .tmux.conf"
-    rm -rf ~/.tmux.conf
-fi
+echo "remove .tmux.conf"
+rm -rf ~/.tmux.conf
