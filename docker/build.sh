6a83387c0026f6c7f9586ed310d80400f4a965c1
cliff xuan <cliff.xuan@gmail.com>
2015-03-25 14:11:12 +0000

    take key as args

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
+if [[ -z $1 ]]
+then
+    KEY='foo'
+    KEY=~/.ssh/id_rsa
+else
+    if [[ -a $1 ]]
+    then
+        KEY=$1
+    else
+        echo "Private key \"$1\" does not exist"
+        exit
+    fi
+fi
+echo $KEY
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-cp ~/.ssh/id_rsa .
+cp $KEY id_rsa
 docker build -t base --no-cache .
