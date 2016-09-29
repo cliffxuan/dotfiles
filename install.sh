6839110b5b51b3d3d72a388a769c47cb1e6cf4d1
Cliff Xuan <cliff.xuan@gmail.com>
2016-09-29 11:03:32 +0000

    more installs

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 BASE_DIR=$(cd "$(dirname "$0")"; pwd)
 
-apt-get install -y zsh
+apt-get update
+apt-get install -y zsh build-essential python-dev nodejs npm
+ln -sf /usr/bin/nodejs /usr/bin/node
+curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
+python /tmp/get-pip.py
+pip install virtualenv virtualenvwrapper
 $BASE_DIR/kickoff.sh
