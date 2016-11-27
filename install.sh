9f43c080272b1a95bfddf3e42f3b6aaf31c19c92
Cliff Xuan <cliff.xuan@gmail.com>
2016-11-27 08:48:20 +0800

    curl

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 BASE_DIR=$(cd "$(dirname "$0")"; pwd)
 
 apt-get update
-apt-get install -y zsh build-essential python-dev nodejs npm
+apt-get install -y curl zsh build-essential python-dev nodejs npm
 ln -sf /usr/bin/nodejs /usr/bin/node
 curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
 python /tmp/get-pip.py
