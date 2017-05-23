fcb4810eb1698e52fff97108279974edd7fea664
Cliff Xuan <cliff.xuan@gmail.com>
2017-05-23 10:59:26 +0000

    use sudo

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 BASE_DIR=$(cd "$(dirname "$0")"; pwd)
 
-apt-get update
-apt-get install -y curl zsh build-essential python-dev python3-dev nodejs npm libffi-dev
-ln -sf /usr/bin/nodejs /usr/bin/node
+sudo apt-get update
+sudo apt-get install -y curl zsh build-essential python-dev python3-dev nodejs npm libffi-dev silversearcher-ag
+sudo ln -sf /usr/bin/nodejs /usr/bin/node
 curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
-python /tmp/get-pip.py
-pip install virtualenv virtualenvwrapper
+sudo python /tmp/get-pip.py
+sudo pip install virtualenv virtualenvwrapper
 $BASE_DIR/kickoff.sh
