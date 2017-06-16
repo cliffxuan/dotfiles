e9007a1186a4db0b149b6502616b0a38cfea55f2
Cliff Xuan <cliff.xuan@gmail.com>
2017-06-16 21:42:13 +0000

    node7

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 BASE_DIR=$(cd "$(dirname "$0")"; pwd)
 
 sudo apt-get update
+curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 sudo apt-get install -y curl zsh build-essential python-dev python3-dev nodejs npm libffi-dev silversearcher-ag
 sudo ln -sf /usr/bin/nodejs /usr/bin/node
 curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
