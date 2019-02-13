d521a5df1654987e6091b2682fd68b5299dd4c9f
Cliff Xuan <cliff.xuan@gmail.com>
2019-02-13 15:16:18 +0000

    python3.7.2

--- install.sh
+++ install.sh
@@ -11,9 +11,9 @@ sudo apt-get install -y libxml2-dev libxslt-dev
 # openssl deps
 sudo apt-get install -y libssl-dev libffi-dev
 
-# install python3.6
-cd /tmp; curl -O https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tgz; tar -xf Python-3.7.0.tgz
-cd /tmp/Python-3.7.0; ./configure; make; sudo make install
+# install python3.7
+cd /tmp; curl -O https://www.python.org/ftp/python/3.7.2/Python-3.7.2.tgz; tar -xf Python-3.7.2.tgz
+cd /tmp/Python-3.7.2; ./configure; make; sudo make install
 sudo ln -sf /usr/bin/nodejs /usr/bin/node
 curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
 sudo python /tmp/get-pip.py
