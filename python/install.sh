ed265c1037dde297485f8533ea31be3c4e8abf2d
Cliff Xuan <cliff.xuan@gmail.com>
2020-03-23 21:34:55 +0000

    build latest python

--- python/install.sh
+++ python/install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # build and install python3
-PY_VERSION=3.8.0
+version=3.8.2
 # python deps
 sudo apt-get install -y python3-dev libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev zlib1g-dev
 # openssl deps
@@ -8,8 +8,11 @@ sudo apt-get install -y libssl-dev libffi-dev
 # lxml deps
 sudo apt-get install -y libxml2-dev libxslt-dev
 
-cd /tmp; curl -O https://www.python.org/ftp/python/$PY_VERSION/Python-$PY_VERSION.tgz; tar -xf Python-$PY_VERSION.tgz
-cd /tmp/Python-$PY_VERSION; ./configure; make; sudo make install
+cd /tmp; curl -O https://www.python.org/ftp/python/$version/Python-$version.tgz; tar -xf Python-$version.tgz
+cd /tmp/Python-$version; ./configure --enable-optimizations --enable-shared; make; sudo make install
+# for LD_LIBRARY_PATH
+echo /usr/local/lib > /etc/ld.so.conf.d/python-$version.conf
+ldconfig
 
 # install pip
 curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
