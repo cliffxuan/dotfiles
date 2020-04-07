ecb1120e641e1b09a757a3613d23f4d61a1f13bd
Cliff Xuan <cliff.xuan@gmail.com>
2020-04-07 18:21:14 +0200

    use sudo when necessary

--- python/install.sh
+++ python/install.sh
@@ -11,8 +11,8 @@ sudo apt-get install -y libxml2-dev libxslt-dev
 cd /tmp; curl -O https://www.python.org/ftp/python/$version/Python-$version.tgz; tar -xf Python-$version.tgz
 cd /tmp/Python-$version; ./configure --enable-optimizations --enable-shared; make; sudo make install
 # for LD_LIBRARY_PATH
-echo /usr/local/lib > /etc/ld.so.conf.d/python-$version.conf
-ldconfig
+echo /usr/local/lib > sudo tee -a /etc/ld.so.conf.d/python-$version.conf
+sudo ldconfig
 
 # install pip
 curl https://bootstrap.pypa.io/get-pip.py -o /tmp/get-pip.py
