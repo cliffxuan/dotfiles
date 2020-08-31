dbc1e558c29b21192bbb1cc512e1a45e2b6a725d
Cliff Xuan <cliff.xuan@gmail.com>
2020-08-31 22:17:10 +0100

    go modules

---
+++ modules/55-go-modules.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# shellcheck source=../../utils.sh
+source "$DIR/../utils.sh"
+
+
+declare -A packages
+packages=( \
+  ["lf"]="gokcehan/lf" \
+  ["gotetris"]="jjinux/gotetris" \
+  ["lazygit"]="jesseduffield/lazygit" \
+  ["lazydocker"]="jesseduffield/lazydocker" \
+)
+
+GOPATH=${GOPATH:-$HOME/.go}
+
+run() {
+  for pkg in "${packages[@]}"
+  do
+    go get "github.com/$pkg"
+  done
+}
+
+check() {
+  for exe in "${!packages[@]}"
+  do
+    [ -x "$GOPATH/bin/$exe" ] || return 1
+  done
+}
+
+provision "$@"
