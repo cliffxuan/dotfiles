f8c1bebfc909fc17f8b5abe061173141449eb080
Cliff Xuan <cliff.xuan@gmail.com>
2020-11-15 12:53:05 +0100

    lzma for python

--- modules/ubuntu/20-python-deps.sh
+++ modules/ubuntu/20-python-deps.sh
@@ -4,5 +4,7 @@
 sudo apt-get install -y python3-dev libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev zlib1g-dev
 # openssl deps
 sudo apt-get install -y libssl-dev libffi-dev
+#lzma
+sudo apt-get install -y lzma lzma-dev liblzma-dev
 # lxml deps
 sudo apt-get install -y libxml2-dev libxslt-dev
