2d90276bbc13a0f0b5ccb47e893c0eb7d1d9dddf
Cliff Xuan <cliff.xuan@gmail.com>
2021-07-24 13:55:21 +0100

    go platform specific

--- modules/50-go.sh
+++ modules/50-go.sh
@@ -3,17 +3,24 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # shellcheck source=../utils.sh
 source "$DIR/../utils.sh"
 
-version=1.14.2
+get_os  # set $OS
+if [[ $OS =~ Darwin ]]; then
+  format="darwin-amd64"
+else
+  format="linux-amd64"
+fi
+
+version=${GO_VERSION:-1.16.5}
 run() {
   cd /tmp || return 1
-  curl -OL "https://dl.google.com/go/go${version}.linux-amd64.tar.gz"
-  sudo tar -C /usr/local -xzf "go${version}.linux-amd64.tar.gz"
+  curl -OL "https://dl.google.com/go/go${version}.${format}.tar.gz"
+  sudo tar -C /usr/local -xzf "go${version}.${format}.tar.gz"
   sudo ln -s /usr/local/go/bin/go /usr/local/bin
   sudo ln -s /usr/local/go/bin/gofmt /usr/local/bin/gofmt
 }
 
 check() {
-  go version 2>&1 | grep -q "go version go$version linux/amd64"
+  go version 2>&1 | grep -q "go version go$version"
 }
 
 
