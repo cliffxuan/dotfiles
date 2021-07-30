bdb02a174e59a27b021bf29ac6858ada8322dfc1
Cliff Xuan <cliff.xuan@gmail.com>
2021-07-30 11:01:45 +0100

    variable for python prefix

--- modules/01-python.sh
+++ modules/01-python.sh
@@ -3,15 +3,16 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # shellcheck source=../utils.sh
 source "$DIR/../utils.sh"
 version=${PYTHON_VERSION:-3.9.5}
+prefix=${PYTHON_PREFIX:-/usr/local}
 
 
 run() {
-  sudo PYTHON_CONFIGURE_OPTS="--enable-shared" python-build "$version" /usr/local
+  sudo PYTHON_CONFIGURE_OPTS="--enable-shared" python-build "$version" "$prefix"
 }
 
 
 check() {
-  python3 -c "import sys; print(sys.version)" 2>&1 | grep -q "$version"
+  "$prefix/bin/python3" -c "import sys; print(sys.version)" 2>&1 | grep -q "$version"
 }
 
 
