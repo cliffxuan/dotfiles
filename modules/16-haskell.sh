1c5089f2bfacd3142470e246ac5c6c3c21737b61
Cliff Xuan <cliff.xuan@gmail.com>
2021-09-16 20:28:14 +0100

    softlink packages

--- modules/16-haskell.sh
+++ modules/16-haskell.sh
@@ -5,28 +5,26 @@ source "$DIR/../utils.sh"
 # shellcheck source=/dev/null
 source "$HOME/.nix-profile/etc/profile.d/nix.sh"
 
-get_executable() {
-  local pkg=$1
-  case $pkg in
-    "inotify-tools")
-      executable=inotifywait
-      ;;
-    "ripgrep")
-      executable=rg
-      ;;
-    *)
-      executable=$pkg
-      ;;
-  esac
-  export executable
+softlink_package_dir() {
+  # https://github.com/haskell/cabal/issues/6262
+  # https://gitlab.haskell.org/ghc/ghc/-/issues/17341
+  cabal v1-install haskell-say
+  PACKAGE_CONF_D=$(ghc-pkg list | grep "$HOME")
+  PACKAGE_DB=$(find ~/.cabal -name package.db -type d)
+  rm -rf "$PACKAGE_CONF_D"
+  ln -s "$PACKAGE_DB" "$PACKAGE_CONF_D"
 }
 
 run() {
   nix-env -i -f '<nixpkgs>' -E 'pkgs: (pkgs {}).ghc.withPackages (hp: with hp; [ zlib ])'
+  nix-env -iA nixpkgs.haskellPackages.cabal-install
+  cabal update
+  softlink_package_dir
 }
 
 check() {
   hash ghc 2>/dev/null || return 1
+  hash cabal 2>/dev/null || return 1
 }
 
 
