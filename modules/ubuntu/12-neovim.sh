999eb9f21433a09ee6b26723b66df558537da08a
Cliff Xuan <cliff.xuan@gmail.com>
2021-12-02 20:04:26 +0000

    neovim for arm

--- modules/ubuntu/12-neovim.sh
+++ modules/ubuntu/12-neovim.sh
@@ -5,11 +5,30 @@ source "$DIR/../../utils.sh"
 
 version="${NVIM_VERSION-0.5.0}"
 
-run() {
+appimage() {
   cd /tmp || exit
   curl -OL "https://github.com/neovim/neovim/releases/download/v$version/nvim.appimage"
   chmod +x nvim.appimage
   sudo mv nvim.appimage /usr/local/bin/nvim
+}
+
+build() {
+  cd /tmp || exit
+  curl -OL "https://github.com/neovim/neovim/archive/refs/tags/v$version.tar.gz"
+  tar xf "v$version.tar.gz"
+  sudo apt-get install -y ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
+  cd "neovim-$version" || exit
+  make -j4
+  sudo make install
+}
+
+run() {
+  if dpkg --print-architecture | grep -q arm64
+  then
+    build
+  else
+    appimage
+  fi
 }
 
 check() {
