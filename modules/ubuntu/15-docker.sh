d6ceb0b3f66db219a74d76bf81d000c6307e9094
Cliff Xuan <cliff.xuan@gmail.com>
2021-12-02 22:12:27 +0000

    updated docker install module

--- modules/ubuntu/15-docker.sh
+++ modules/ubuntu/15-docker.sh
@@ -4,11 +4,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$DIR/../../utils.sh"
 
 run() {
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   sudo apt-get update
-  apt-cache policy docker-ce
-  sudo apt-get install -y docker-ce
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 }
 
 check() {
