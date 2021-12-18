6c60489ef81d29faad118afeda2c3895132225b3
Cliff Xuan <cliff.xuan@gmail.com>
2021-12-18 10:33:50 +0000

    rust

---
+++ modules/60-rust.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# shellcheck source=../utils.sh
+source "$DIR/../utils.sh"
+
+run() {
+  curl https://sh.rustup.rs -sSf | sh -s -- -y
+}
+
+check() {
+  rustc
+}
+
+provision "$@"
