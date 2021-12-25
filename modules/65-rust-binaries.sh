b647895eb70b1d068e6e6f913571e98e040803cd
Cliff Xuan <cliff.xuan@gmail.com>
2021-12-25 21:57:27 +0000

    zoxide

--- modules/65-rust-binaries.sh
+++ modules/65-rust-binaries.sh
@@ -4,12 +4,24 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$DIR/../utils.sh"
 
 
+declare -A packages
+packages=( \
+  ["cargo-add"]="cargo-edit" \
+  ["zoxide"]="zoxide" \
+)
+
 run() {
-  cargo install cargo-edit
+  for pkg in "${packages[@]}"
+  do
+    cargo install "$pkg" --locked
+  done
 }
 
 check() {
-  "$HOME/.cargo/bin/cargo add"
+  for exe in "${!packages[@]}"
+  do
+    [ -x "$HOME/.cargo/bin/$exe" ] || return 1
+  done
 }
 
 provision "$@"
