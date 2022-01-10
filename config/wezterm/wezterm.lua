6a84af0a0032c9e55e2888aa3179edbc3414fb90
Cliff Xuan <cliff.xuan@gmail.com>
2022-01-10 12:02:38 +0000

    updated keybinding for werzterm

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -8,10 +8,12 @@ return {
   keys = {
     {key="LeftArrow", mods="SUPER", action=wezterm.action{SendString="\x00\x70"}}, -- ctrl-space p
     {key="RightArrow", mods="SUPER", action=wezterm.action{SendString="\x00\x6e"}}, -- ctrl-space n
-    {key="LeftArrow", mods="SUPER|SHIFT", action=wezterm.action{SendString="\x00\x68"}}, -- ctrl-space h
-    {key="RightArrow", mods="SUPER|SHIFT", action=wezterm.action{SendString="\x00\x6c"}}, -- ctrl-space l
-    {key="UpArrow", mods="SUPER|SHIFT", action=wezterm.action{SendString="\x00\x6b"}}, -- ctrl-space k
-    {key="DownArrow", mods="SUPER|SHIFT", action=wezterm.action{SendString="\x00\x6a"}}, -- ctrl-space j
+    {key="d", mods="SUPER", action=wezterm.action{SendString="\x00\x25"}}, -- ctrl-space %
+    {key="d", mods="SUPER|SHIFT", action=wezterm.action{SendString="\x00\x22"}}, -- ctrl-space "
+    {key="LeftArrow", mods="SUPER|SHIFT", action=wezterm.action{SendString="\x00\x48"}}, -- ctrl-space H
+    {key="RightArrow", mods="SUPER|SHIFT", action=wezterm.action{SendString="\x00\x4c"}}, -- ctrl-space L
+    {key="UpArrow", mods="SUPER|SHIFT", action=wezterm.action{SendString="\x00\x4b"}}, -- ctrl-space K
+    {key="DownArrow", mods="SUPER|SHIFT", action=wezterm.action{SendString="\x00\x4a"}}, -- ctrl-space J
     {key="x", mods="SUPER", action=wezterm.action{SendString="\x00\x5b"}}, -- ctrl-space [
   },
   window_padding = {
@@ -22,5 +24,5 @@ return {
   },
   -- color_scheme = "ayu",
   -- color_scheme = "DotGov",
-  color_scheme = "Dracula",
+  -- color_scheme = "Dracula",
 }
