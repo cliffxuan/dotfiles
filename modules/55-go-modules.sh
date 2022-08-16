e0677c70d95d660a8e7d51c12d1f200e5ae277f8
Cliff Xuan <cliff.xuan@gmail.com>
2022-08-16 12:19:39 +0200

    shfmt

--- modules/55-go-modules.sh
+++ modules/55-go-modules.sh
@@ -1,29 +1,27 @@
 #!/usr/bin/env bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # shellcheck source=../utils.sh
 source "$DIR/../utils.sh"
 
-
 declare -A packages
-packages=( \
-  ["lf"]="gokcehan/lf" \
-  ["lazygit"]="jesseduffield/lazygit" \
-  ["bt"]="cliffxuan/bt" \
-  ["lazydocker"]="jesseduffield/lazydocker"
+packages=(
+  ["lf"]="github.com/gokcehan/lf"
+  ["lazygit"]="jgithub.com/esseduffield/lazygit"
+  ["bt"]="github.com/cliffxuan/bt"
+  ["lazydocker"]="github.com/jesseduffield/lazydocker"
+  ["shfmt"]="mvdan.cc/sh/v3/cmd/shfmt"
 )
 
 GOPATH=${GOPATH:-$HOME/.go}
 
 run() {
-  for pkg in "${packages[@]}"
-  do
-    go install "github.com/$pkg@latest"
+  for pkg in "${packages[@]}"; do
+    go install "$pkg@latest"
   done
 }
 
 check() {
-  for exe in "${!packages[@]}"
-  do
+  for exe in "${!packages[@]}"; do
     [ -x "$GOPATH/bin/$exe" ] || return 1
   done
 }
