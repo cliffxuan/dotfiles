8d888aa0fe1147f6637cacb1a7e009a68924423d
Cliff Xuan <cliff.xuan@gmail.com>
2022-10-08 23:33:09 +0100

    better color

--- scripts/run-pytest.py
+++ scripts/run-pytest.py
@@ -8,14 +8,37 @@ import re
 import subprocess
 import typing as t
 
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-YELLOW = (255, 255, 0)
 
-
-def color_text(text: str, rgb: t.Tuple[int, int, int]) -> str:
-    r, g, b = rgb
-    return f"\033[38;2;{r};{g};{b}m{text}\033[0m"
+def color_print(text: str, color: str = ""):
+    mapping = {
+        "black": (0, 0, 0),
+        "white": (255, 255, 255),
+        "red": (255, 0, 0),
+        "lime": (0, 255, 0),
+        "blue": (0, 0, 255),
+        "yellow": (255, 255, 0),
+        "cyan": (0, 255, 255),
+        "magenta": (255, 0, 255),
+        "silver": (192, 192, 192),
+        "gray": (128, 128, 128),
+        "maroon": (128, 0, 0),
+        "olive": (128, 128, 0),
+        "green": (0, 128, 0),
+        "purple": (128, 0, 128),
+        "teal": (0, 128, 128),
+        "navy": (0, 0, 128),
+    }
+    if color:
+        try:
+            r, g, b = mapping[color]
+            print(f"\033[38;2;{r};{g};{b}m{text}\033[0m")
+            return
+        except KeyError:
+            print(
+                f'Unsupported color "{color}", use one of "{", ".join(mapping.keys())}".'
+            )
+    else:
+        print(text)
 
 
 def argument_parser():
@@ -43,14 +66,14 @@ def main(argv=None):
     for line in lines[line_number - 1 : 0 : -1]:
         func = get_test_function(line)
         if func:
-            subprocess.call(["pytest", f"{args.filename.name}::{func}", "-vv"])
+            cmd = ["pytest", f"{args.filename.name}::{func}", "-vv"]
+            color_print(" ".join(cmd), "green")
+            subprocess.call(cmd)
             break
     else:
-        print(
-            color_text(
-                f'no test found for "{args.filename.name}" from line "{args.line_number}" above',
-                RED,
-            )
+        color_print(
+            f'no test found for "{args.filename.name}" from line "{args.line_number}" above',
+            "red",
         )
 
 
