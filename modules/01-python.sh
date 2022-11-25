37c7631d0f782a2c434c96ef42a3703701443564
Cliff Xuan <cliff.xuan@gmail.com>
2022-11-25 23:06:31 +0000

    python 3.11

--- modules/01-python.sh
+++ modules/01-python.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # shellcheck source=../utils.sh
 source "$DIR/../utils.sh"
-version=${PYTHON_VERSION:-3.10.6}
+version=${PYTHON_VERSION:-3.11.0}
 prefix=${PYTHON_PREFIX:-$HOME/.pyenv/versions/$version}
 
 
