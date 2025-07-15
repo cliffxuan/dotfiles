755e5a887cf45b08b04324e3fd1153a8b05315c8
Cliff Xuan <cliff.xuan@gmail.com>
2025-07-15 21:01:41 +0000

    updated spaceship.zsh

--- zsh/spaceship.zsh
+++ zsh/spaceship.zsh
@@ -1,2 +1,1 @@
-  zinit ice src"spaceship.zsh"; zinit light denysdovhan/spaceship-prompt
-  eval spaceship_vi_mode_enable
+zinit ice src"spaceship.zsh"; zinit light spaceship-prompt/spaceship-prompt
