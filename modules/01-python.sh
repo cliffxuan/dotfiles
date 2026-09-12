7150246f2cbebfaba0dd91f9f2ab6d57b3a14b86
Cliff Xuan <cliff.xuan@gmail.com>
2026-09-12 19:14:26 +0000

    feat(python): link default python executables via uv --default

--- modules/01-python.sh
+++ modules/01-python.sh
@@ -5,11 +5,11 @@ source "$DIR/../utils.sh"
 version=${PYTHON_VERSION:-3.14}
 
 run() {
-  uv python install "$version"
+  uv python install --default "$version"
 }
 
 check() {
-  uv python find "$version" >/dev/null 2>&1
+  uv python find "$version" >/dev/null 2>&1 && [[ "$("$HOME/.local/bin/python" --version 2>&1)" =~ Python\ $version ]]
 }
 
 provision "$@"
